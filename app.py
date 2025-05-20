56ff1d7fc044a702b25ed4d85ef0efd48c3b4c7f
AdelGamalEGI <adel.gamal@eliagrid-int.com>
2025-05-20 14:35:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def member_card(name, role):
     )
 
 # Build Dash app
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+app = dash.Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.BOOTSTRAP])
 server = app.server
 
 
